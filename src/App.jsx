d6211f7ceb273e3f2b627268fb686526ddfb172c
Pedro Rosique <rosiquemurciapedro@gmail.com>
2024-12-06 19:34:27 +0100

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './components/Navigation/Navigation.css'
 import Navigation from './components/Navigation'
 import AppRoutes from './routes/AppRoutes.jsx'
 import MusicPlayer from './components/MusicPlayer/MusicPlayer.jsx'
+import UserMessage from './components/UserMessage/UserMessage.jsx'
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navigation />
       <MusicPlayer />
       <AppRoutes />
+      <UserMessage />
     </div>
   )
 }
