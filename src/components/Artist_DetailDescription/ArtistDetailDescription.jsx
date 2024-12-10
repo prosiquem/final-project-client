894e0c0049f77f8f4018a38828ddb55ce2649846
efrontinan <elenauxui@gmail.com>
2024-12-10 20:09:05 +0100

    Artist Details Page & Components

--- src/components/Artist_DetailDescription/ArtistDetailDescription.jsx
+++ src/components/Artist_DetailDescription/ArtistDetailDescription.jsx
@@ -1,30 +1,127 @@
-import { Col, Row, Form, FloatingLabel } from "react-bootstrap"
+import { Col, Row, Form, FloatingLabel, Badge, Button } from "react-bootstrap"
+import { MUSIC_GENRES } from "../../consts/music.consts"
+import { Icon } from "../GeneralIconBootstrap/GeneralIconBootstrap"
+import { NavLink, useNavigate } from "react-router-dom"
+import Select from "react-select"
+import makeAnimated from "react-select/animated"
+import { SOCIAL_MEDIA } from "../../consts/user.consts"
+import { XLg } from "react-bootstrap-icons"
 
-const ArtistDetailsDescription = ({ data, loggedUser, isEditing, setIsEditing }) => {
+const ArtistDetailsDescription = ({ data, loggedUser, isEditing, socialMediaData, setSocialMediaData }) => {
+
+    const navigate = useNavigate()
+    const animatedComponents = makeAnimated()
 
     return (
         <Form className="ArtistDetailsDescription">
 
             <Row>
-                <Col md="8" >
-                    <Form.Group>
-                        <FloatingLabel
-                            controlId="description"
-                            label="Sobre ti"
-                            className={!isEditing && ""}>
-                            <Form.Control
-                                as="textarea"
-                                rows={!isEditing && 6}
-                                plaintext={!isEditing}
-                                readOnly={!isEditing}
-                                defaultValue={data.description} />
-                        </FloatingLabel>
-                    </Form.Group>
+                <Col md={{ offset: "1" }}>
+                    {isEditing ?
+                        <Form.Group className="mb-3">
+                            <FloatingLabel
+                                controlId="description"
+                                label="Sobre ti"
+                                className={!isEditing && ""}>
+
+                                <Form.Control
+                                    as="textarea"
+                                    rows={6}
+                                    className="text-area"
+                                    defaultValue={data.description} />
+                            </FloatingLabel>
+                        </Form.Group>
+                        :
+                        <p>{data.description}</p>
+                    }
+
+                    {isEditing ?
+
+                        <Form.Group className="mb-3">
+                            <Select
+                                className="select-form"
+                                classNamePrefix="select"
+                                components={animatedComponents}
+                                placeholder="Género musical"
+                                name="musicGenres"
+                                options={MUSIC_GENRES}
+                                value={MUSIC_GENRES.filter(option => data.musicGenres.includes(option.value))}
+
+                                isMulti
+                            />
+                        </Form.Group>
+                        :
+                        data.musicGenres.map(elm => {
+                            return (
+                                <Badge>{elm}</Badge>
+                            )
+                        })
+                    }
                 </Col>
 
-                <Col>
-                </Col>
+                {isEditing ?
+                    <Col md={{ span: "4", offset: "1" }}>
+                        <Form.Group>
+                            {data.socialMedia.map((elm, idx) => {
+                                return (
+                                    <Row className="gap-2" key={`musicGenre-${idx}`}>
+                                        <Col md="4" className="p-0">
+                                            <Form.Select className="mb-3">
+                                                {SOCIAL_MEDIA.map((elm, idx) => {
+                                                    return (
+                                                        <option key={idx} value={[elm.value, elm.icon]}>{elm.label}</option>
+                                                    )
+                                                })}
+                                            </Form.Select>
+                                        </Col>
+                                        <Col className="p-0">
+                                            <Form.Group className="mb-3">
+                                                <Form.Control
+                                                    type="text"
+                                                    name="social-media-input"
+                                                    placeholder="URL"
+                                                    value={elm.url}
+                                                    id={`formSocialMedia-${idx}`}
+                                                />
+                                            </Form.Group>
+                                        </Col>
+                                        <Col md="1" className="p-0">
+                                            <Button
+                                                variant="custom-transparent"
+                                                disabled={socialMediaData.length <= 1}
+                                                size="sm">
+                                                <XLg color="#a8a8a8" />
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                )
+                            })
+                            }
+                            <Button
+                                variant="custom-transparent"
+                                size="sm"
+                                className="mt-2">
+                                Añadir red social
+                            </Button>
+                        </Form.Group>
+                    </Col>
+                    :
+                    <Col md={{ span: "1", offset: "2" }}>
+                        {data.socialMedia.map(elm => {
+                            return (
+                                <NavLink variant="custom-transparent" to={elm.url}>
+                                    <Icon
+                                        iconName={elm.icon}
+                                        size={28} />
+                                </NavLink>
+                            )
+                        })}
+
+                    </Col>
+                }
             </Row>
+
+
 
         </Form>
     )
--- src/pages/ArtistDetailsPage/ArtistDetailsPage.jsx
+++ src/pages/ArtistDetailsPage/ArtistDetailsPage.jsx
@@ -2,12 +2,16 @@ import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import userServices from "../../services/user.services"
 import Loader from "../../components/Loader/Loader"
-import { Carousel, Container, Image } from "react-bootstrap"
+import { Button, Carousel, Col, Container, Image, Row } from "react-bootstrap"
 import ArtistDetailsHeader from "../../components/Artist_DetailsHeader/ArtistDetailsHeader"
 import { AuthContext } from "../../contexts/auth.context"
 
 import './ArtistDetailsPage.css'
 import ArtistDetailsDescription from "../../components/Artist_DetailDescription/ArtistDetailDescription"
+import ArtistGalleryForm from "../../components/ArtistGalleryForm/ArtistGalleryForm"
+import albumServices from "../../services/album.services"
+import AlbumList from "../../components/AlbumList/AlbumList"
+import AlbumCard from "../../components/AlbumCard/AlbumCard"
 
 const ArtistDetailsPage = () => {
 
@@ -18,26 +22,47 @@ const ArtistDetailsPage = () => {
     const [artist, setArtist] = useState()
     const [isLoading, setIsLoading] = useState(true)
     const [isEditing, setIsEditing] = useState(false)
+    const [artistAlbum, setArtistAlbums] = useState()
+
+
+    const [socialMediaData, setSocialMediaData] = useState([
+        {
+            socialMedia: "Youtube",
+            url: "",
+            icon: "Youtube"
+        }
+    ])
 
     useEffect(() => {
 
-        fetchArtist(artistId)
+        fetchData(artistId)
 
-    }, [artistId])
+    }, [artistId, isEditing])
 
-    const fetchArtist = (artistId) => {
+    const fetchData = (artistId) => {
+        const artistData = [
+            userServices.fetchUser(artistId),
+            albumServices.fetchArtistAlbum(artistId)
+        ]
 
-        userServices
-            .fetchUser(artistId)
-            .then(({ data }) => {
-                setArtist(data)
+        Promise.all(artistData)
+            .then(([{ data: artistData }, { data: albumArtistData }]) => {
+                setArtist(artistData)
+                setSocialMediaData(artistData.socialMedia)
+                setArtistAlbums(albumArtistData)
                 setIsLoading(false)
             })
+            .catch(err => console.log(err))
+
+    }
+
+    const editArtist = (artistId) => {
+        console.log(artist)
     }
 
     return (isLoading ? <Loader /> :
         <div className="ArtistDetailsPage">
-            <Container className="page-container gap-4">
+            <Container className="page-container">
 
                 <Carousel fade controls={false} indicators={false} interval={4000} className="coverCarousel">
                     {artist.artistGallery.map(elm => {
@@ -51,15 +76,39 @@ const ArtistDetailsPage = () => {
 
                 <ArtistDetailsHeader
                     data={artist}
+                    changeData={setArtist}
                     loggedUser={loggedUser}
                     isEditing={isEditing}
-                    setIsEditing={setIsEditing} />
+                    setIsEditing={setIsEditing}
+                    editArtist={editArtist} />
 
                 <ArtistDetailsDescription
                     data={artist}
+                    changeData={setArtist}
                     loggedUser={loggedUser}
                     isEditing={isEditing}
-                    setIsEditing={setIsEditing} />
+                    socialMediaData={socialMediaData}
+                    setSocialMediaData={setSocialMediaData} />
+
+                {isEditing && <ArtistGalleryForm />}
+
+                {!isEditing && <Row className="py-5">
+                    <Col md={{ offset: "1" }}>
+                        <h2>Discografía</h2>
+                        {
+                            artistAlbum.length <= 0 ? < Loader /> :
+                                <AlbumList albums={artistAlbum} />
+                        }
+                    </Col>
+                </Row>}
+
+                {isEditing &&
+                    <Button
+                        onClick={() => { setIsEditing(false), editArtist() }}
+                        variant="custom-primary">
+                        Guardar
+                    </Button>}
+
 
             </Container>
         </div>
