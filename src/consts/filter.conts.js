d333c60485c95f20abcd940c01d85d2a4783613c
Pedro Rosique <rosiquemurciapedro@gmail.com>
2025-01-10 00:14:33 +0100

    minor changes

--- src/consts/filter.conts.js
+++ src/consts/filter.conts.js
@@ -1,6 +1,6 @@
 export const EXPLORE_FILTERS = [
     { value: "all", label: "Todos" },
     { value: "artists", label: "Artistas" },
-    { value: "playlists", label: "Playlist" },
+    { value: "playlists", label: "Playlists" },
     { value: "albums", label: "Álbumes" }
 ]
